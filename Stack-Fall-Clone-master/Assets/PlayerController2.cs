7379ec9c2f29adeaab8fa4f863e932277a8416fa
Abdulkadir Gür <58683068+AbdulkadirGur@users.noreply.github.com>
2022-10-23 05:27:54 +0300

    .

--- Stack-Fall-Clone-master/Assets/PlayerController2.cs
+++ Stack-Fall-Clone-master/Assets/PlayerController2.cs
@@ -6,9 +6,14 @@ public class PlayerController2 : MonoBehaviour
 {
     public Rigidbody rb;
     bool carpma;
-    void Start()
+
+    private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+    }
+    void Start()
+    {
+        
     }
 
     // Update is called once per frame
@@ -39,6 +44,17 @@ public class PlayerController2 : MonoBehaviour
         if (!carpma)
         {
             rb.velocity = new Vector3(0, 50 * Time.deltaTime * 5, 0);
+        }
+        else
+        {
+            if (collision.gameObject.tag=="enemy")
+            {
+                Destroy(collision.gameObject);
+            }
+            else if (collision.gameObject.tag == "plane")
+            {
+                Debug.Log("GameOver");
+            }
         }
     }
     private void OnCollisionStay(Collision collision)
